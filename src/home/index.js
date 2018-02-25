c843f2fe2a01dfb176a85179d0308b0183e5059b
Kristofer Rye <kristofer.rye@gmail.com>
2018-02-25 16:03:54 -0600

    Start sprucing up the app

--- src/home/index.js
+++ src/home/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import FontAwesome from 'react-fontawesome'
 import GoogleLogin from 'react-google-login'
 import postTokenToServer from '../lib/auth'
 import {CLIENT_ID} from '../config'
@@ -15,19 +16,20 @@ class Home extends React.Component {
 
 	render() {
 		return (
-				<div>
-					<p> Welcome to Oleville Voting! </p>
-					<p> Please log in to access your ballot. </p>
-					<GoogleLogin
-						clientId={CLIENT_ID}
-						buttonText='Login'
-						responseType='token'
-						hostedDomain='stolaf.edu'
-						onSuccess={this.googleResponseSuccess}
-						onFailure={this.googleResponseFailure}
-					/>
-				</div>
-			)
+			<div>
+			<p> Welcome to Oleville Voting! </p>
+			<p> Please log in to access your ballot. </p>
+			<GoogleLogin clientId={CLIENT_ID}
+			buttonText='Login'
+			responseType='token'
+			hostedDomain='stolaf.edu'
+			onSuccess={this.googleResponseSuccess}
+			onFailure={this.googleResponseFailure}
+      >
+			<FontAwesome name='google' /> <span> Sign in with Google </span>
+			</GoogleLogin>
+			</div>
+		)
 	}
 }
 
