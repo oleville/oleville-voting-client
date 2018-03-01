c9d6a9048bd4cf2e31554ad4a53df7902a523ac8
Kristofer Rye <kristofer.rye@gmail.com>
2018-02-28 20:36:28 -0600

    Remove the header stuff
    For now I'm nixing it because it doesn't do anything and it stresses
    me out.

--- src/header/index.js
+++ src/header/index.js
@@ -4,12 +4,8 @@ class Header extends React.Component {
 	render() {
 		return (
 			<div>
-				<h2> Home </h2>
-				<h2> Live </h2>
-				<h2> Admin </h2>
 			</div>
 		)
 	}
 }
-
 export default Header
